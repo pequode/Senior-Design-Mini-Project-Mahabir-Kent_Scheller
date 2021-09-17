d602ebe3d4dc43426793e876650bc04e06268b2c
tottto45 <georgetks@gmail.com>
2021-09-16 23:27:43 -0400

    small fix

--- hello_world/screens/SignIn.js
+++ hello_world/screens/SignIn.js
@@ -19,12 +19,12 @@ import { Headline, Paragraph,TouchableRipple  } from 'react-native-paper';
 import { useIsFocused } from '@react-navigation/core';
 import { useNavigation } from '@react-navigation/native';
 import auth from '@react-native-firebase/auth';
-import { GoogleSignin } from '@react-native-community/google-signin';
+import { GoogleSignin, statusCodes } from '@react-native-community/google-signin';
 
 
 const SignIn =({ navigation }) => {
     // const user = auth().currentUser;
-    // const [authenticated, setAutheticated] = useState(false);
+     const [authenticated, setAutheticated] = useState(false);
     // GoogleSignin.configure({webClientId:'515008457162-mjfsucblgofg46bub2nlirhpraamq8lu.apps.googleusercontent.com',});
     // async function onGoogleButtonPress() {
     //     const { idToken } = await GoogleSignin.signIn();
@@ -43,18 +43,27 @@ const SignIn =({ navigation }) => {
     GoogleSignin.configure({webClientId:'515008457162-mjfsucblgofg46bub2nlirhpraamq8lu.apps.googleusercontent.com',});
     signIn = async () => {
         try {
+            console.log("fcuk12")
           await GoogleSignin.hasPlayServices();
+          console.log("fcuk123")
           const userInfo = await GoogleSignin.signIn();
-          this.setState({ userInfo });
+          console.log("fcuk32")
+          setAutheticated({ userInfo });
+          console.log("dones")
         } catch (error) {
+            console.log("fcuk")
           if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+            console.log("fcuk1")
             // user cancelled the login flow
           } else if (error.code === statusCodes.IN_PROGRESS) {
+            console.log("fcuk2")
             // operation (e.g. sign in) is in progress already
           } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+            console.log("fcuk3")
             // play services not available or outdated
           } else {
             // some other error happened
+            console.log(error)
           }
         }
       };
