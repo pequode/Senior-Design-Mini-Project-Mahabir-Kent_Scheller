ecebd7b8deecaa59fb7caf7c47b003662c6faeb7
tottto45 <georgetks@gmail.com>
2021-09-17 21:34:19 -0500

    Update sayHi.py

--- docs/test/sayHi.py
+++ docs/test/sayHi.py
@@ -1,7 +1,7 @@
 print("hello running Test Suite")
 import os
 import sys
-directory = os.listdir("../../")
+directory = os.listdir("../../hello_world")
 print(directory)
 print(os.environ['API_KEY_USDA'])
 print("if above is *** api aquired!!!!")
@@ -9,13 +9,16 @@ check = os.environ['API_KEY_USDA'];
 print(len(check))
 searchString = check
 foundAPI=False
-# for fname in directory: # change directory as needed
-#     if searchString in fname:
-#         f = open(fname,'r')
-#         print('found string in file %s') %fname
-        #   foundAPI=True
-        #   break
-#     else:
-#         print('string not found')
+for fname in directory: # change directory as needed
+    if searchString in fname:
+        f = open(fname,'r')
+        print('found string in file %s') %fname
+        foundAPI=True
+        break
+    else:
+        print('string not found')
 if(foundAPI):
     print("Warring api key present")
+    os.environ["found_Key"] = "1"
+else:
+   os.environ["found_Key"] = "0" 
