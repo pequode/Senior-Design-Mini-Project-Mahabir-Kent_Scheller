b703f57759cc5bcd43095184f8d678e5ae4b56cb
tottto45 <georgetks@gmail.com>
2021-09-17 21:29:01 -0500

    made changes to automation

--- docs/test/sayHi.py
+++ docs/test/sayHi.py
@@ -1,3 +1,21 @@
 print("hello running Test Suite")
 import os
+import sys
+directory = os.listdir("../../")
+print(directory)
 print(os.environ['API_KEY_USDA'])
+print("if above is *** api aquired!!!!")
+check = os.environ['API_KEY_USDA'];
+print(len(check))
+searchString = check
+foundAPI=False
+# for fname in directory: # change directory as needed
+#     if searchString in fname:
+#         f = open(fname,'r')
+#         print('found string in file %s') %fname
+        #   foundAPI=True
+        #   break
+#     else:
+#         print('string not found')
+if(foundAPI):
+    print("Warring api key present")
